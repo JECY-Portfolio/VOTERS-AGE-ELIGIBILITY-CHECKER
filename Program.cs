fdfe84717292dc06d150fef63414d4a3e7463b56
Geofrrey Jecinta <120319299+JECY-Portfolio@users.noreply.github.com>
2025-03-25 22:36:46 +0100

    Add files via upload
    Added voter eligibility checker program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("How old are you: ");
+        int age;
+
+        while (!int.TryParse(Console.ReadLine(), out age) || age <= 0)
+        {
+            Console.Write("Invalid input: please enter a POSITIVE number: ");
+        }
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are eligible to vote.");
+        }
+        else
+        {
+            Console.WriteLine("You are less than 18 years old and NOT eligible to vote.");
+        }
+    }
+}
